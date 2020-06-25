b4ed0190916666bb9604a89878f4043c99d6984b
John Todd <jtodd2854@gmail.com>
2020-06-25 11:08:22 -0400

    refactor

--- src/main/java/org/jtodd/poker/Game.java
+++ src/main/java/org/jtodd/poker/Game.java
@@ -23,7 +23,6 @@ public class Game {
             Player player = new Player(comps[0], ranking);
             players.add(player);
         }
-        Collections.sort(players, Comparator.comparing(p -> p.ranking));
         return new Game(players);
     }
 
@@ -42,13 +41,13 @@ public class Game {
                 .stream()
                 .collect(Collectors.groupingBy(p -> p.ranking.getValue()))
                 .entrySet().stream()
-                .min((e1, e2) -> e1.getKey().compareTo(e2.getKey()))
+                .min(Map.Entry.comparingByKey())
                 .get()
                 .getValue();
         if (firstPlace.size() == 1) {
             return Optional.of(firstPlace.get(0));
         } else {
-            Collections.sort(firstPlace, (f1, f2) -> f1.ranking.tieBreaker(f2.ranking));
+            firstPlace.sort((f1, f2) -> f1.ranking.tieBreaker(f2.ranking));
             if (firstPlace.get(0).ranking.tieBreaker(firstPlace.get(1).ranking) == 0) {
                 return Optional.empty();
             } else {
